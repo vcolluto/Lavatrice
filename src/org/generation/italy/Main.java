e65d22251cd2293c219b883682b980cbb62be094
Vincenzo Colluto <enzocoll@tiscali.it>
2024-06-10 10:22:25 +0200

    parte 1 completata con Main

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -45,16 +45,79 @@ rinominare il metodo  avviaLavaggio() in avviaProgramma(); il messaggio deve ess
 
 package org.generation.italy;
 
+import java.util.Scanner;
+
 import org.generation.italy.model.Lavatrice;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
 		Lavatrice l=new Lavatrice();
-		l.accendi();
-		System.out.println(l.toString());
+		String scelta;
+		do{
+			System.out.println("\n\n\n\n\n\n\nGestione lavatrice:");
+			System.out.println("1. Accendi");
+			System.out.println("2. Spegni");
+			System.out.println("3. Apri sportello");
+			System.out.println("4. Chiudi sportello");
+			System.out.println("5. Aggiungi detersivo");
+			System.out.println("6. Imposta temperatura");
+			System.out.println("7. Avvia lavaggio");
+			System.out.println("8. Termina lavaggio");
+			System.out.println("0. Esci\n");
+
+			
+			System.out.println(l.toString());	//mostra lo stato della lavatrice
+		
+
+			System.out.print("\nInserisci la tua scelta:");
+			scelta = sc.nextLine();
+
+			switch (scelta) {
+			case "1":		
+				l.accendi();				
+				break;
+			case "2":
+				l.spegni();
+					
+				break;
+			case "3":
+				l.apriSportello();
+				break;
+			case "4":
+				l.chiudiSportello();
+				break;
+			case "5":
+				l.aggiungiDetersivo();
+				break;
+			case "6":
+				System.out.print("Inserisci la temperatura (0-90):");
+				try {
+					int temperatura = Integer.parseInt(sc.nextLine());
+					l.impostaTemperatura(temperatura);
+				} catch (NumberFormatException e) {
+					System.out.println("Temperatura non valida");
+				}
+				break;
+			case "7":
+				l.avviaLavaggio();
+				break;
+			case "8":
+				l.terminaLavaggio();
+				break;
+			
+			case "0":
+				System.out.println("Arrivederci!");
+			default:
+				System.out.println("Scelta non valida. Riprova.");
+				break;
+			}
+			System.out.println("\nPremi invio per continuare...");
+			sc.nextLine();
+		} while (!scelta.equals("0"));
+		sc.close();
 		
 		
 
